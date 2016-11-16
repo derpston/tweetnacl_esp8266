c2ae94ed933f9ff31d0281ab2f786d6a41cad0fa
derpston <derpston@example.com>
2016-11-16 21:48:40 +0000

    Adding safe but useless randombytes implementation.

---
+++ randombytes.c
@@ -0,0 +1,6 @@
+void randombytes(unsigned char *x,unsigned long long xlen)
+{
+    // No implemenation of randombytes yet, so at least do something safe
+    // and prevent any use of this.
+    while(1);
+}
